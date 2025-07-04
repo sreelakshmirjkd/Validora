f8cb13f4d6b7669124a4d53e7a75f53aaba216a8
Sreelakshmi <sreelakshmirjkd@gmail.com>
2025-07-04 02:51:52 +0200

    static settings correction

--- ValidoraProject/settings.py
+++ ValidoraProject/settings.py
@@ -187,9 +187,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
